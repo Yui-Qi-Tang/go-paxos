a0872df7f6613471441abefce13b59de255a6b29
yqtang <yqtang1222@gmail.com>
2019-06-26 17:38:43 +0800

    add test case: deaf proposer

--- paxos/paxos_test.go
+++ paxos/paxos_test.go
@@ -1,7 +1,9 @@
 package paxos
 
 import (
+	"os"
 	"testing"
+
 	// "strconv"
 	// "os"
 	"runtime"
@@ -44,6 +46,11 @@ func waitn(t *testing.T, nodes []*Node, seq int, wanted int) {
 	}
 }
 
+func waitmajority(t *testing.T, nodes []*Node, seq int) {
+	// |majority set| = (len(nodes)/2) + 1
+	waitn(t, nodes, seq, (len(nodes)/2)+1)
+}
+
 func TestRunPaxos(t *testing.T) {
 	runtime.GOMAXPROCS(4)
 	t.Logf("Test: Run Paxos ...\n")
@@ -54,7 +61,7 @@ func TestRunPaxos(t *testing.T) {
 
 	// 建立RPC 位置
 	for i := 0; i < numberOfNodes; i++ {
-		rpcAddrs[i] = getPRCAddr("create_test", i)
+		rpcAddrs[i] = getPRCAddr("basic", i)
 	}
 
 	// 建立節點
@@ -93,5 +100,55 @@ func TestRunPaxos(t *testing.T) {
 	}
 
 	t.Logf(" ... Passed\n")
+
+}
+
+func TestNodeDeaf(t *testing.T) {
+	runtime.GOMAXPROCS(4)
+	const numberOfNodes = 5
+	nodes := make([]*Node, numberOfNodes)
+	rpcAddrs := make([]string, numberOfNodes)
+	defer shutAllDown(nodes)
+
+	// 建立RPC 位置
+	for i := 0; i < numberOfNodes; i++ {
+		rpcAddrs[i] = getPRCAddr("deaf", i)
+	}
+
+	// 建立節點
+	for i := 0; i < numberOfNodes; i++ {
+		nodes[i] = createNode(rpcAddrs, i, nil)
+	}
+
+	t.Logf("Test: Deaf proposer ...\n")
+
+	// node 0 proposes 'hey' with proposal seq 0
+	nodes[0].Start(0, "hey")
+	waitn(t, nodes, 0, numberOfNodes)
+
+	// remove node's rpc socket -> the node has been crash
+	os.Remove(rpcAddrs[0])
+	os.Remove(rpcAddrs[numberOfNodes-1])
+
+	// node 1 proposes proposes 'hi' with proposal seq 1
+	nodes[1].Start(1, "hi")
+	waitmajority(t, nodes, 1)
+	time.Sleep(1 * time.Second)
+	if ndecided(t, nodes, 1) != numberOfNodes-2 {
+		t.Fatalf("a deaf peer heard about a decision")
+	}
+
+	// node is deaf which sends proposal
+	nodes[0].Start(1, "xxx")
+	waitn(t, nodes, 1, numberOfNodes-1)
+	time.Sleep(1 * time.Second)
+	if ndecided(t, nodes, 1) != numberOfNodes-1 {
+		t.Fatalf("a deaf peer heard about a decision")
+	}
+
+	nodes[numberOfNodes-1].Start(1, "yyy")
+	waitn(t, nodes, 1, numberOfNodes)
+
+	t.Log("... Passed")
 
 }
